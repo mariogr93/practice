c80f183b70b9e75dc07160645accbb5f6f94919d
mariogr93 <mariogr93@gmail.com>
2020-01-28 02:23:52 -0400

    updated files with exercies and information

--- practice.js
+++ practice.js
@@ -1,1 +1,22 @@
-console.log("hola mundo");
+console.log("hola mundo" +" " + getNumber(5));
+function getNumber(n){
+    var list = [ ];
+    for( n ; n >= 1 ; n-- ){
+        list.push(n);
+    }
+    return list;
+}
+
+/////////////////////////////////////////////////
+
+function divisors(n){
+    const list = [];
+    for(var i = 2; i <= n ; i++){
+        if(n%i === 0){
+            list.push(i);
+        }
+    }
+    return list;
+}
+
+console.log(divisors(26))
--- practice.py
+++ practice.py
@@ -1,1 +1,26 @@
-print("hola mundo")
+
+
+def getNumber(n):
+    lista = []
+    while n >= 1:
+        lista.append(n)
+        n-=1
+    return lista
+
+print(getNumber(5))
+
+#####################################
+
+i = 5
+print([i for i in range(i,0,-1)])
+
+#####################################
+
+def divisors(n):
+    lista = []
+    for i in range(2,n+1):
+        if n%i == 0:
+            lista.append(i)
+    return lista
+
+print(divisors(26))
